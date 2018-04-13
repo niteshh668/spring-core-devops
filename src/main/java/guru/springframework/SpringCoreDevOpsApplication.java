fb8f4f623db3313aae0b2cf9d54fdde48c6e0626
nitesh <nitesh.h668@gmail.com>
2018-04-13 12:26:02 -0500

    testing CI build

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,7 +9,7 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		//this is a whitespace change test
+		//This is a test
 		// another test...
 	}
 }
